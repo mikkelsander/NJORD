c8c0c1b25a8cadbd6d7f11f64561fb2759a18bd5
mikkelsander <mikkelguntveit@gmail.com>
2016-10-06 11:20:42 +0200

    Initial commit

---
+++ app/src/main/java/com/project/ms/njord/MainActivity.java
@@ -0,0 +1,13 @@
+package com.project.ms.njord;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
