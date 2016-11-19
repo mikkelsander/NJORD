1bad60c422659a90186ad5664bd03aee917d266b
Oliver <Oliver.e.t@hotmail.com>
2016-11-20 00:39:14 +0100

    Lavet UI til signUp aktiviteten

---
+++ app/src/main/java/com/project/ms/njord/controller/SessionController.java
@@ -0,0 +1,8 @@
+package com.project.ms.njord.controller;
+
+/**
+ * Created by Oliver on 19-Nov-16.
+ */
+
+public class SessionController {
+}
---
+++ app/src/main/java/com/project/ms/njord/entity/LungTestProgress.java
@@ -0,0 +1,8 @@
+package com.project.ms.njord.entity;
+
+/**
+ * Created by Oliver on 19-Nov-16.
+ */
+
+public class LungTestProgress {
+}
