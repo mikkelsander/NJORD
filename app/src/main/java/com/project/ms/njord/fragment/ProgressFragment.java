da3b62b54553ab07e1412bf0e3796bf12883281f
mikkelsander <mikkelguntveit@gmail.com>
2016-11-23 22:43:00 +0100

    small layout changes to progress fragment

--- app/src/main/java/com/project/ms/njord/fragment/ProgressFragment.java
+++ app/src/main/java/com/project/ms/njord/fragment/ProgressFragment.java
@@ -26,13 +26,13 @@ public class ProgressFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        groups = new String[]{"Test Result 1", "Test Result 2", "Test Result 3", "Test Result 4"};
+        groups = new String[]{"Date: 21-01-2015", "Date: 27-01-2015", "Date: 14-02-2015", "Date: 20-02-2015"};
 
         children = new String[][]{
-                {"s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."},
-                {"Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of comes from a line in section 1.10.32."},
-                {"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."},
-                {"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."}
+                {"Calculated lung level:", "Max Exp.:", "Min Exp.:", "Max Insp.", "Min Insp:", "Time:" },
+                {"Calculated lung level:", "Max Exp.:", "Min Exp.:", "Max Insp.", "Min Insp:", "Time:" },
+                {"Calculated lung level:", "Max Exp.:", "Min Exp.:", "Max Insp.", "Min Insp:", "Time:" },
+                {"Calculated lung level:", "Max Exp.:", "Min Exp.:", "Max Insp.", "Min Insp:", "Time:" },
         };
     }
 
