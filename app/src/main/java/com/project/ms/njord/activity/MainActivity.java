7126cfee368f8834998ac141419078eac5c775a2
madswerner <mads_werner@hotmail.com>
2017-01-02 13:18:21 +0100

    test2

--- app/src/main/java/com/project/ms/njord/activity/MainActivity.java
+++ app/src/main/java/com/project/ms/njord/activity/MainActivity.java
@@ -33,6 +33,8 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        //testtest
+
         prefs = PreferenceManager.getDefaultSharedPreferences(this);
 
 
