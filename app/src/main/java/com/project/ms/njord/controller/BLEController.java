08cf6d92ae9595e9160824e746b21f30e3db7043
mikkelsander <mikkelguntveit@gmail.com>
2017-01-06 10:31:18 +0100

    adding BLEcontroller

---
+++ app/src/main/java/com/project/ms/njord/controller/BLEController.java
@@ -0,0 +1,33 @@
+package com.project.ms.njord.controller;
+
+/**
+ * Created by ms on 06/01/17.
+ */
+
+public class BLEController {
+    private DataHolder  dataHolder;
+
+    public BLEController (){
+        this.dataHolder = new DataHolder();
+    }
+
+    private class DataHolder {
+
+        private int[] dataPoints;
+
+        public void addData(int) {
+            // add int to dataPoints
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
