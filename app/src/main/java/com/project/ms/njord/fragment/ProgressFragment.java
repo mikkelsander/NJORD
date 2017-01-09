19b00de6646bf026bce626f3f6633446e964c8b8
madswerner <mads_werner@hotmail.com>
2017-01-09 10:05:12 +0100

    Delte results + changes in graph

--- app/src/main/java/com/project/ms/njord/fragment/ProgressFragment.java
+++ app/src/main/java/com/project/ms/njord/fragment/ProgressFragment.java
@@ -6,9 +6,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseExpandableListAdapter;
 import android.widget.ExpandableListView;
-import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
@@ -37,15 +35,6 @@ public class ProgressFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_progress, container, false);
 
-        groups = new String[]{"Date: 21-01-2015", "Date: 27-01-2015", "Date: 14-02-2015", "Date: 20-02-2015"};
-
-        children = new String[][]{
-                {"Calculated lung le vel:", "Max Exp.:", "Min Exp.:", "Max Insp.", "Min Insp:", "Time:" },
-                {"Calculated lung level:", "Max Exp.:", "Min Exp.:", "Max Insp.", "Min Insp:", "Time:" },
-                {"Calculated lung level:", "Max Exp.:", "Min Exp.:", "Max Insp.", "Min Insp:", "Time:" },
-                {"Calculated lung level:", "Max Exp.:", "Min Exp.:", "Max Insp.", "Min Insp:", "Time:" },
-        };
-
 //lav array som indeholder tryk og dato
         GraphView graph = (GraphView) rootView.findViewById(R.id.fragment_progress_graph);
 
@@ -67,9 +56,9 @@ public class ProgressFragment extends Fragment {
             }
         });
 
-        graph.setTitle("Your Progress");
+        graph.setTitle("");
 
-        //highligts points
+        //highlights points
         series.isDrawDataPoints();
 
         // activate horizontal zooming and scrolling
@@ -77,28 +66,26 @@ public class ProgressFragment extends Fragment {
 
         // activate horizontal scrolling
         graph.getViewport().setScrollable(true);
-
         graph.getViewport().setScalableY(true);
-
         graph.getViewport().setScrollableY(true);
 
 
-
-
         // set manual X bounds
-        //graph.getViewport().setXAxisBoundsManual(false);
-        //graph.getViewport().setMinX(0);
-        //graph.getViewport().setMaxX(100);
+        graph.getViewport().setXAxisBoundsManual(false);
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(20);
 
         // set manual Y bounds
-        //graph.getViewport().setYAxisBoundsManual(false);
-        //graph.getViewport().setMinY(0);
-        //graph.getViewport().setMaxY(30);
+        graph.getViewport().setYAxisBoundsManual(false);
+        graph.getViewport().setMinY(0);
+        graph.getViewport().setMaxY(20);
 
         graph.addSeries(series);
 
         return rootView;
     }
+
+
 
     private void replaceFragment(Fragment someFragment) {
         FragmentTransaction transaction = getFragmentManager().beginTransaction();
@@ -112,106 +99,5 @@ public class ProgressFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        lv = (ExpandableListView) view.findViewById(R.id.expListView);
-        lv.setAdapter(new ExpandableListAdapter(groups, children));
-        lv.setGroupIndicator(null);
 
-    }
-
-
-
-    public class ExpandableListAdapter extends BaseExpandableListAdapter {
-
-        private final LayoutInflater inf;
-        private String[] groups;
-        private String[][] children;
-
-        public ExpandableListAdapter(String[] groups, String[][] children) {
-            this.groups = groups;
-            this.children = children;
-            inf = LayoutInflater.from(getActivity());
-        }
-
-        @Override
-        public int getGroupCount() {
-            return groups.length;
-        }
-
-        @Override
-        public int getChildrenCount(int groupPosition) {
-            return children[groupPosition].length;
-        }
-
-        @Override
-        public Object getGroup(int groupPosition) {
-            return groups[groupPosition];
-        }
-
-        @Override
-        public Object getChild(int groupPosition, int childPosition) {
-            return children[groupPosition][childPosition];
-        }
-
-        @Override
-        public long getGroupId(int groupPosition) {
-            return groupPosition;
-        }
-
-        @Override
-        public long getChildId(int groupPosition, int childPosition) {
-            return childPosition;
-        }
-
-        @Override
-        public boolean hasStableIds() {
-            return true;
-        }
-
-        @Override
-        public View getChildView(int groupPosition, final int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-
-            ViewHolder holder;
-            if (convertView == null) {
-                convertView = inf.inflate(R.layout.expandable_list_results_items, parent, false);
-                holder = new ViewHolder();
-
-                holder.text = (TextView) convertView.findViewById(R.id.lblListItem);
-                convertView.setTag(holder);
-            } else {
-                holder = (ViewHolder) convertView.getTag();
-            }
-
-            holder.text.setText(getChild(groupPosition, childPosition).toString());
-
-            return convertView;
-        }
-
-        @Override
-        public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-            ViewHolder holder;
-
-            if (convertView == null) {
-                convertView = inf.inflate(R.layout.expandable_list_results_groups, parent, false);
-
-                holder = new ViewHolder();
-                holder.text = (TextView) convertView.findViewById(R.id.lblListHeader);
-                convertView.setTag(holder);
-            } else {
-                holder = (ViewHolder) convertView.getTag();
-            }
-
-            holder.text.setText(getGroup(groupPosition).toString());
-
-            return convertView;
-        }
-
-        @Override
-        public boolean isChildSelectable(int groupPosition, int childPosition) {
-            return true;
-        }
-
-        private class ViewHolder {
-            TextView text;
-        }
-    }
-}
+}}
