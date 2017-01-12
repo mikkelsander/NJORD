b064ee1fc520b7a5738e2984eb9e6e16645b137d
madswerner <mads_werner@hotmail.com>
2017-01-12 10:41:45 +0100

    preogress

--- app/src/main/java/com/project/ms/njord/activity/ResultsActivity.java
+++ app/src/main/java/com/project/ms/njord/activity/ResultsActivity.java
@@ -1,16 +1,18 @@
 package com.project.ms.njord.activity;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.project.ms.njord.R;
 
 public class ResultsActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button doneBtn;
+    TextView inhale, exhale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,14 @@ public class ResultsActivity extends AppCompatActivity implements View.OnClickLi
 
         doneBtn = (Button) findViewById(R.id.activity_results_doneBtn);
         doneBtn.setOnClickListener(this);
+
+        inhale = (TextView) findViewById(R.id.activity_results_inhale_avg);
+        inhale.setText("Inhale: 54!");
+
+
+        exhale = (TextView) findViewById(R.id.activity_results_exhale_avg);
+        exhale.setText("Exhale: 65!");
+
     }
 
     @Override
