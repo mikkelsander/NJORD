23fd69d7c16248b0afa300dbe48120af8177ddb2
GloriousHypnotoad <s153643@student.dtu.dk>
2017-01-15 11:54:32 +0100

    First commit from Simon. Added 3x notifcation Java Classes and modified Manifest.

---
+++ app/src/main/java/com/project/ms/njord/notifications/AlarmListener.java
@@ -0,0 +1,46 @@
+package com.project.ms.njord.notifications;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+
+import com.project.ms.njord.R;
+import com.project.ms.njord.activity.MainActivity;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
+
+/**
+ * Created by simon on 15-01-2017.
+ */
+
+public class AlarmListener extends BroadcastReceiver {
+
+    private NotificationManager notificationManager = null;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        notificationManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, new Intent(context, MainActivity.class), 0);
+
+        Notification notification = new Notification.Builder(context)
+                .setContentTitle("Så er det tid til at blæse!")
+                .setContentText("Blæs, mester, blæs!")
+                .setSmallIcon(R.drawable.ic_tv_dark)
+                .setWhen(System.currentTimeMillis())
+                .setAutoCancel(true)
+                .setCategory(Notification.CATEGORY_ALARM)
+                .setContentIntent(pendingIntent)
+                .setOnlyAlertOnce(true)
+                .setVibrate(new long[]{1000L, 500L, 1000L, 500L, 1000L})
+                .setSound(Settings.System.DEFAULT_NOTIFICATION_URI)
+                .build();
+
+        notificationManager.notify(0, notification);
+    }
+}
