8d81f6f167bdb2e0980826732d592ee029772c1c
madswerner <mads_werner@hotmail.com>
2017-01-16 13:06:35 +0100

    youtube

--- app/src/main/java/com/project/ms/njord/activity/YouTubeActivity.java
+++ app/src/main/java/com/project/ms/njord/activity/YouTubeActivity.java
@@ -16,9 +16,10 @@ public class YouTubeActivity extends YouTubeBaseActivity
             implements YouTubePlayer.OnInitializedListener{
 
     private static final String TAG = "YouTubeActivity";
-    static final String GOOGLE_API_KEY = "AIzaSyDhYktHkqag3wu67GkktSMsAYKjBPW_WDs";
-    static final String YOUTUBE_VIDEO_ID = "El69SuPPFa0";
-   // static final String YOUTUBE_PLAYLIST = "PLXtTjtWmQhg1SsviTmKkWO5n0a_-T0bnD";
+    static final String GOOGLE_API_KEY2 = "AIzaSyDhYktHkqag3wu67GkktSMsAYKjBPW_WDs";
+    public String YOUTUBE_VIDEO_ID;
+    // static final String YOUTUBE_PLAYLIST = "PLXtTjtWmQhg1SsviTmKkWO5n0a_-T0bnD";      "El69SuPPFa0"
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +32,10 @@ public class YouTubeActivity extends YouTubeBaseActivity
         YouTubePlayerView playerView = new YouTubePlayerView(this);
         playerView.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT));
         layout.addView(playerView);
-        playerView.initialize(GOOGLE_API_KEY,this);
+        playerView.initialize(GOOGLE_API_KEY2,this);
+
+        Bundle bundle = getIntent().getExtras();
+        YOUTUBE_VIDEO_ID = bundle.getString("Address");
 
     }
 
