af73bdb94f621ac1ed340463cec34c94bcdc3c7b
GloriousHypnotoad <s153643@student.dtu.dk>
2017-01-16 12:02:56 +0100

    Sixth commit fra Simon. Brugeren bliver kun opfordret til at give tilladelse til push efter første POWER TEST.

--- app/src/main/java/com/project/ms/njord/activity/ResultsActivity.java
+++ app/src/main/java/com/project/ms/njord/activity/ResultsActivity.java
@@ -1,11 +1,16 @@
 package com.project.ms.njord.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.project.ms.njord.R;
 
@@ -13,6 +18,8 @@ public class ResultsActivity extends AppCompatActivity implements View.OnClickLi
 
     Button doneBtn;
     TextView inhale, exhale;
+    SharedPreferences sharedPref;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,16 +37,49 @@ public class ResultsActivity extends AppCompatActivity implements View.OnClickLi
         exhale = (TextView) findViewById(R.id.activity_results_exhale_avg);
         exhale.setText("Exhale: 65!");
 
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        editor = sharedPref.edit();
+
     }
 
     @Override
     public void onClick(View v) {
         if (v == doneBtn) {
             Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
             finish();
+
+            //Check if this is the first run of the app, and if so call for dialog
+            if (sharedPref.getBoolean("firstRun", true)) {
+                showNotificationAlertDialog();
+            }
 
         }
 
     }
 
+    //Generates Dialog asking for Push notification permission
+    public void showNotificationAlertDialog() {
+        editor.putBoolean("firstRun", false).commit();
+        new AlertDialog.Builder(this)
+                .setTitle("Notifications")
+                .setMessage("Training on a regular bases is important. AEROFIT would like permission to send you daily reminders." +
+                        "\n\n" +
+                        "You can always change your selection in the menu under Preferences.")
+                .setPositiveButton("Activate Notifications", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        showConfirmNotificationAlertDialog();
+                    }
+                }).setNegativeButton("No thanks", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        }).show();
+    }
+
+    public void showConfirmNotificationAlertDialog() {
+        Toast.makeText(this, "Notifications activated", Toast.LENGTH_LONG)
+                .show();
+    }
 }
