8d33c373c232e25bb36f6617bc0768483c9f044c
Oliver <Oliver.e.t@hotmail.com>
2017-01-17 14:24:53 +0100

    results

--- app/src/main/java/com/project/ms/njord/activity/ResultsActivity.java
+++ app/src/main/java/com/project/ms/njord/activity/ResultsActivity.java
@@ -5,14 +5,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.project.ms.njord.R;
 import com.project.ms.njord.fragment.SettingsFragment;
 
@@ -44,29 +42,7 @@ public class ResultsActivity extends AppCompatActivity implements View.OnClickLi
         inhale = (TextView) findViewById(R.id.activity_results_inhale_avg);
         inhale.setText(Integer.toString(bundle.getInt("inhale")));
         exhale = (TextView) findViewById(R.id.activity_results_exhale_avg);
-//        exhale.setText(bundle.getString("exhale").toString());
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (v == doneBtn) {
-
-            //Check if this is the first run of the app, and if so call for dialog
-            //if (sharedPref.getBoolean("firstRun", true)) {
-            showNotificationAlertDialog();
-            //}else{
-            //Intent i = new Intent(this, MainActivity.class);
-            //startActivity(i);
-            //finish();
-            //}
-
-
-
-
-
-        }
-
-
+        exhale.setText(Integer.toString(bundle.getInt("exhale")));
     }
 
     //Generates Dialog asking for Push notification permission. If given all permissions are switched on with standard prefs.
@@ -103,4 +79,19 @@ public class ResultsActivity extends AppCompatActivity implements View.OnClickLi
         Toast.makeText(this, "Notifications activated", Toast.LENGTH_LONG)
                 .show();
     }
+
+    @Override
+    public void onClick(View v) {
+        if (v == doneBtn) {
+            //Check if this is the first run of the app, and if so call for dialog
+            //if (sharedPref.getBoolean("firstRun", true)) {
+            showNotificationAlertDialog();
+            //}else{
+            //Intent i = new Intent(this, MainActivity.class);
+            //startActivity(i);
+            //finish();
+            //}
+        }
+    }
+
 }
