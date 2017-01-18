f82def873e7440f417ed963b1c9b142b6fe81305
madswerner <mads_werner@hotmail.com>
2017-01-18 22:30:31 +0100

    Musik + slider

--- app/src/main/java/com/project/ms/njord/fragments/DeStressFragment.java
+++ app/src/main/java/com/project/ms/njord/fragments/DeStressFragment.java
@@ -1,10 +1,10 @@
 package com.project.ms.njord.fragments;
 
 
-import android.content.res.AssetFileDescriptor;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,15 +12,12 @@ import android.widget.Button;
 
 import com.project.ms.njord.R;
 
-import java.io.IOException;
-
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DeStressFragment extends Fragment implements View.OnClickListener {
 
 
-    final MediaPlayer mp = new MediaPlayer();
     Button danish,english;
 
     public DeStressFragment() {
@@ -31,35 +28,32 @@ public class DeStressFragment extends Fragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_de_stress, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_de_stress, container, false);
 
-        danish = (Button) v.findViewById(R.id.destress_danish_button);
-        danish.setOnClickListener(this);
+        danish = (Button) rootView.findViewById(R.id.destress_danish_button);
 
-        return v;
+        english = (Button) rootView.findViewById(R.id.destress_english_button);
+
+        final MediaPlayer sound = MediaPlayer.create(getActivity(), R.raw.aerofiteng);
+
+        danish.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                sound.setLooping(true);
+                Log.d("sound", "playing sound");
+                sound.start();
+            }
+        });
+
+        return rootView;
 
     }
 
     @Override
     public void onClick(View v) {
 
-        if(mp.isPlaying())
-        {
-            mp.stop();
-        }
 
-        try {
-            mp.reset();
-            AssetFileDescriptor afd;
-        //    afd = getAssets().openFd("Aerofit- ny modstand -musik.mp3");
-         //   mp.setDataSource(afd.getFileDescriptor(),afd.getStartOffset(),afd.getLength());
-            mp.prepare();
-            mp.start();
-        } catch (IllegalStateException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
     }
 }
