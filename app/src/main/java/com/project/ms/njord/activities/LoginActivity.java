2e8dd6f455713ad7a8f207855fa88c8ef0554f9c
Oliver <Oliver.e.t@hotmail.com>
2017-01-18 20:34:28 +0100

    fullscreen i login

--- app/src/main/java/com/project/ms/njord/activities/LoginActivity.java
+++ app/src/main/java/com/project/ms/njord/activities/LoginActivity.java
@@ -2,6 +2,9 @@ package com.project.ms.njord.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.project.ms.njord.R;
 import com.project.ms.njord.fragments.LoginFragment;
@@ -12,8 +15,13 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_login);
 
+        getSupportActionBar().hide();
         if (savedInstanceState == null)
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.login_fragment_container, new LoginFragment())
